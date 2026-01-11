b6d5531ff55d6beaab12de6f82f8b1d39443775a
test-aitest <tatukidayo@icloud.com>
2026-01-11 20:13:15 +0900

    Add Two Sum solution for LeetCode testing

---
+++ sample/two_sum.py
@@ -0,0 +1,15 @@
+# Two Sum Solution
+# LeetCode Problem: https://leetcode.com/problems/two-sum/
+
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in seen:
+                return [seen[complement], i]
+            seen[num] = i
+        return []
