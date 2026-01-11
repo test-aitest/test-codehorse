a2a7808eb9533c5472ba23ffd6e050a002641c98
test-aitest <tatukidayo@icloud.com>
2026-01-11 11:58:04 +0900

    claude code とスプレッドシートの機能を削除

--- packages/codehorse-handler/bin/codehorse-handler.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-
-import("../dist/index.js").catch((err) => {
-  console.error("Error loading codehorse-handler:", err.message);
-  console.error("\nPlease build the package first: npm run build");
-  process.exit(1);
-});
