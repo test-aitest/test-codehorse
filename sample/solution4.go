e3ae0b39a3b4c4cfd4b8cc65f52da92adba1116e
test-aitest <tatukidayo@icloud.com>
2026-01-12 12:50:28 +0900

    テスtp

--- sample/solution4.go
+++ sample/solution4.go
@@ -2,7 +2,7 @@ package main
 
 type Solution4 struct{}
 
-func (s *Solution) TwoSum4(nums []int, target int) []int {
+func (s *Solution4) TwoSum4(nums []int, target int) []int {
 	seen := make(map[int]int)
 	for i, num := range nums {
 		if j, ok := seen[target-num]; ok {
